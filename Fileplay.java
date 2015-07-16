d041b73c815ad7515585a2f005ab6a0716fd1aff
hehuan1993 <707926049@qq.com>
2015-07-16 20:12:29 +0800

    文件操作

--- Fileplay.java
+++ Fileplay.java
@@ -1,4 +1,4 @@
-import java.io.File;
+/*import java.io.File;
 import java.io.FileInputStream;
 import java.io.InputStream;
 public class Fileplay {
@@ -14,5 +14,32 @@ public class Fileplay {
 		System.out.print(tempt);
 	}
 	{
+	}
+}*/
+/*import java.io.File;
+public class Fileplay{
+	public static void main(String[] args) throws Exception {
+		File file = new File ("D:\\ttttt");
+		file.createNewFile();
+	}
+}*/
+/*import java.io.File;
+import java.io.FileOutputStream;
+import java.io.OutputStream;
+
+public class Fileplay {
+	public static void main(String[] args) throws Exception {
+		String s = "Hello world!";
+		File file = new File("D:\\ttt");
+		OutputStream out = new FileOutputStream(file);
+		out.write(s.getBytes());// 字符串转换为字节数组
+		out.close();
+	}
+}*/
+import java.io.File;
+public class Fileplay {
+	public static void main(String[] args) throws Exception {
+		File file = new File("D:\\ttt");
+		file.delete();
 	}
 }
