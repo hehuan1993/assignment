8976c8b911b16bc1884d29288dd66098d8d5604c
hehuan1993 <707926049@qq.com>
2015-07-17 12:11:39 +0800

    test

---
+++ Marchar1.java
@@ -0,0 +1,17 @@
+package maxchar1;
+
+import java.util.Scanner;
+
+
+public class Maxchar {
+public static void main(String[] args){
+	char str[]; 
+	System.out.print("ÇëÊäÈë×Ö·ûÊı×é");
+	Scanner input = new Scanner(System.in);
+    str[]= input.nextInt();
+	Csj.guess(n);
+   
+}
+
+
+}
