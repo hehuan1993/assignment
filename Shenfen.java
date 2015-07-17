186cae018fcce81f2dd7190ff8dacc9eeabdedcc
hehuan1993 <707926049@qq.com>
2015-07-17 19:49:36 +0800

    验证身份证

--- Shenfen.java
+++ Shenfen.java
@@ -1,54 +1,85 @@
+package jiaoyanshenfen;
+import java.util.Scanner;
 public class Shenfen {
-	public static void main(String[] args) throws Exception {
-		String [] s; 
-		System.out.print("请输入字符串");
-		Scanner input = new Scanner(System.in);
-		s=input.next();
-		int[] a = { 7, 9, 10, 5, 8, 4, 2, 1, 6, 3, 7, 9, 10, 5, 8, 4, 2 };
-		int sum = 0;
-		for (int i = 0; i < a.length; i++) {
-			sum += Character.digit(s.length, 10) * a[i];
-			System.out.println("Totally sum：" + sum);
-			int mod = sum % 11;
-			System.out.println("mod:" + mod);
-			switch (mod) {
-			case 0:
-				System.out.print("1");
-				break;
-			case 1:
-				System.out.print("0");
-				break;
-			case 2:
-				System.out.print("x");
-				break;
-			case 3:
-				System.out.print("9");
-				break;
-			case 4:
-				System.out.print("8");
-				break;
-			case 5:
-				System.out.print("7");
-				break;
-			case 6:
-				System.out.print("6");
-				break;
-			case 7:
-				System.out.print("5");
-				break;
-			case 8:
-				System.out.print("4");
-				break;
-			case 9:
-				System.out.print("3");
-				break;
-			case 10:
-				System.out.print("2");
-				break;
-
-			}
-
+	public static boolean checkId(String[] array) {
+		Integer[] a = new Integer[17];
+		for (int k = 0; k < 17; k++) {
+			a[k] = Integer.parseInt(array[k]);
 		}
+		int sum = 0;
+		int[] b = { 7, 9, 10, 5, 8, 4, 2, 1, 6, 3, 7, 9, 10, 5, 8, 4, 2 };
+		for (int j = 0; j < 17; j++) {
+			sum += a[j] * b[j];
+		}
+		int mod = sum % 11;
+		boolean c = false;
+		switch (mod) {
+		case 0:
+			if ("1".equals(array[17]))
+				c = true;
+			break;
+		case 1:
+			if ("0".equals(array[17]))
+				c = true;
+			break;
+		case 2:
+			if ("X".equals(array[17]))
+				c = true;
+			break;
+		case 3:
+			if ("9".equals(array[17]))
+				c = true;
+			break;
+		case 4:
+			if ("8".equals(array[17]))
+				c = true;
+			break;
+		case 5:
+			if ("7".equals(array[17]))
+				c = true;
+			break;
+		case 6:
+			if ("6".equals(array[17]))
+				c = true;
+			break;
+		case 7:
+			if ("5".equals(array[17]))
+				c = true;
+			break;
+		case 8:
+			if ("4".equals(array[17]))
+				c = true;
+			break;
+		case 9:
+			if ("3".equals(array[17]))
+				c = true;
+			break;
+		case 10:
+			if ("2".equals(array[17]))
+				c = true;
+			break;
+		}
+
+		return c;
+
+	}
+
+	public static void main(String[] args) throws Exception {
+		String s;
+		System.out.println("请输入18位身份证号码");
+		Scanner input = new Scanner(System.in);
+		s = input.next();
+		if (s.length() != 18)
+			System.out.println("请重新输入");
+		String[] p = new String[s.length()];
+		for (int i = 0; i < s.length(); i++) {
+			p[i] = (s.charAt(i) + "");
+		}
+		if (checkId(p))
+			System.out.println("身份证合法");
+		else
+			System.out.println("身份证不合法");
 
 	}
 }
+	
